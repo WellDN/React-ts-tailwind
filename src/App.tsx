b9d187beb5e366e49e1a639114d44292d590dccd
Wellington Silva <Wellingtontp0@gmail.com>
2022-07-23 20:16:52 -0300

    Outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,48 @@
-  function App () {
-    return (
-      <body className="flex flex-col gap-4 fixed">
+import { Route, Routes, Outlet } from 'react-router-dom'
+import { Store } from './pages/Store'
+import { About } from './pages/About'
+
+function Home () {
+  return (
+    <body className="flex flex-col gap-4 fixed">
     <nav className="flex items-center pl-4 bg-gray-300 h-12 min-w-max">
-      <div className="inline-flex h-full items-center">
+    <div className="inline-flex h-full items-center">
     <a href="/" className="mr-8">
-        Home
+    Home
     </a>
     <a href="/store" className="mr-8">
-        Store
-        </a>
-    <a href="/about" className="">
-        About
+    Store
     </a>
-    </div>
-    </nav>
-    <div className="flex h-full w-full pl-2">
-    <h1 className="text-7xl">Home</h1>
-    </div>
-    <section>
-    <div className="grid gap-4 grid-cols-3 grid-rows-2 h-screen w-screen">
+    <a href="/about" className="">
+    About
+</a>
+</div>
+</nav>
+<div className="flex h-full w-full pl-2">
+<h1 className="text-7xl">Home</h1>
+</div>
+<section>
+<div className="grid gap-4 grid-cols-3 grid-rows-2 h-screen w-screen">
+</div>
+</section>
+<Outlet />
+</body>
+  )
+}
 
-    </div>
-    </section>
-    </body>
-    )
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} >
+      <Route path="about" element={<About />}/>
+      <Route element={<Home/>} />
+      <Route path="/store" element={<Store />} />
+      <Route path="/about" element={<About />} />
+      </Route>
+      </Routes>
+      );
     }
-        
+
+      
+
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom' //create different pages
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
     <App />
     </BrowserRouter>
-  </React.StrictMode> // the BrowserRouter have to cover the whole website.
+  </React.StrictMode>
 )
