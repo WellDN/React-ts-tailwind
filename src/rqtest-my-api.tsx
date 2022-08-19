3903d81f7418c505f05801190e2a7e9e4d391848
Wellington Silva <Wellingtontp0@gmail.com>
2022-08-19 19:14:15 -0300

    react query t

---
+++ src/rqtest-my-api.tsx
@@ -0,0 +1,17 @@
+type RqTest = {
+    todo: string
+    
+}
+    type TodoType = {
+        id: number
+        title: string
+        todo: string     
+}                 //just fake shit to fetch things
+
+export function getTodos({todo}: RqTest) {
+    
+}
+
+export function postTodo() {
+    
+}
