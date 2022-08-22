542f62ecf0d887028daec78384ddfc30b030593d
Wellington Silva <Wellingtontp0@gmail.com>
2022-08-22 19:24:21 -0300

    array id

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,17 +10,16 @@ import { useShoppingCart } from '../hooks/context'
 
 export function Home() {
   const { cartQuantity } = useShoppingCart()
+
   return (
-    <>
-    {storeItems.map(item => (   //the map take the entire array
-      <body className="grid gap-5 min-w-min bg-white">
+    <body className="grid gap-5 min-w-min bg-white">
      <nav className="flex px-2  bg-gray-300 h-12">
      <div className="flex justify-center w-full text-lg text-gray-500">
      <div className="w-full justify-center flex"></div>
      <div className="pt-2">
     <Link
      to="/" className="pr-20 hover:text-gray-900">
-      Home
+       Home
     </Link>
     <Link to="/store" className="pr-20 hover:text-gray-900">
       Store
@@ -35,14 +34,15 @@ export function Home() {
   <Link to="/store" className="w-5">
   {cartQuantity > 0 && ( // change with the add cart button count
    <svg viewBox='0 0 20 20' className="">
-   <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13
+     <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13
     12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5
     12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7
     0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"></path>
    </svg>
     //!!!!important  on the cartquantity is the transform to make a cart quantity absolute with the cart use as reference
     )}</Link></div>
-    <div className="absolute translate-y-0 w-2">
+    
+    <div className="absolute translate-y-0 w-4">
     <div className="absolute translate-y-0 pointer-events-none">{cartQuantity}</div>
    </div>
    </div>
@@ -60,7 +60,7 @@ export function Home() {
       <div className="flex justify-center"><span className=""></span></div>
     <div className="flex h-50 w-full justify-center">
     <div className="pt-16">
-    <HomeItem {...item}/>
+      <HomeItem id={1}/>
       </div> 
     </div>
   </div>
@@ -68,31 +68,26 @@ export function Home() {
     <img src={landscape2} alt="idkk" className="h-96 w-full"/>
       <div className="flex justify-center"></div>
       <div className="flex pt-16 justify-center">
-      <Link to="/store" className="text-1xl text-slate-600 hover:text-stone-800"><button className='rounded-full pb-1 pr-2 pl-2 border-2 border-slate-600 hover:border-slate-900'>
-        Add to Cart</button></Link>
+      <HomeItem id={2}/>
       </div>
   </div>
   <div className="h-full w-full">
       <img src={landscape3} alt="idkkkk" className="h-96 w-full"/>
       <div className="flex justify-center"></div>
       <div className="flex pt-16 justify-center">
-      <Link to="/store" className="text-1xl text-slate-600 hover:text-stone-800"><button className='rounded-full pb-1 pr-2 pl-2 border-2 border-slate-600 hover:border-slate-900'>
-        Add to Cart</button></Link>
+      <HomeItem id={3}/>
   </div>
   </div>
   <div className="h-full w-full">
       <img src={landscape4} alt="idkkkkkk" className="h-96 w-full"/>
       <div className="flex justify-center"></div>
       <div className="flex pt-16 justify-center h-60 w-50">
-        <Link to="/store" className="text-1xl text-slate-600 hover:text-stone-800"><button className='rounded-full pb-1 pr-2 pl-2 border-2 border-slate-600 hover:border-slate-900'>
-        Add to Cart</button></Link>
+      <HomeItem id={4}/>
   </div>
   </div>
   </div>
   </div>
   </section>
-  </body> 
-  ))}
-</>
+  </body>
   )
 }
